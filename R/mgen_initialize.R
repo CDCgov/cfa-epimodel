78857900e490790cc66ee897c0be069ca7deeafd
Emily Pollock <ruu7@cdc.gov>
2025-08-04 19:16:12 +0000

    move cfa-uw-modeling/epimodel-sti to this repo

---
+++ R/mgen_initialize.R
@@ -0,0 +1,12 @@
+#' @title Initialization Model for Mgen Model
+#'
+#' @description Initialize networks and starting nodal attributes
+#'
+#' @param dat simulation object
+#' @param at time step
+#'
+#' @export
+
+initialize_mgen <- function(dat, at) {
+  return(dat) # nolint
+}
---
+++ R/mgen_tx.R
@@ -0,0 +1,11 @@
+#' @title Mgen Treatment
+#'
+#' @description treatment of mgen
+#'
+#' @inheritParams initialize_mgen
+#'
+#' @export
+
+tx_mgen <- function(dat, at) {
+  return(dat) # nolint
+}
