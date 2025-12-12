646c2a20611ed97f8a5261de423a625d9b2f2f82
Emily Pollock <ruu7@cdc.gov>
2025-12-12 20:57:01 +0000

    add additional test case for init network attrs

--- tests/testthat/test-initialize_sim.R
+++ tests/testthat/test-initialize_sim.R
@@ -1,11 +1,17 @@
 # Load test network fit object
 fit <- readRDS(test_path("input", "test_nw.RDS"))
 
-# Create a version of the fit object with a missing attribute for testing
+# Create a version of the fit object with a missing attribute for testing (that should fail)
 nw_missing_attr <- fit$newnetwork
 network::delete.vertex.attribute(nw_missing_attr, "age")
 fit_with_missing_attr <- fit
 fit_with_missing_attr$newnetwork <- nw_missing_attr
+
+# Created a version with an additional attribute (that should pass)
+nw_additional_attr <- fit$newnetwork
+network::set.vertex.attribute(nw_additional_attr, "new_attr", rep(1, network::network.size(nw_additional_attr)))
+fit_with_additional_attr <- fit
+fit_with_additional_attr$newnetwork <- nw_additional_attr
 
 # Prep input functions
 params <- EpiModel::param.net()
@@ -29,5 +35,10 @@ test_that("mod_sti_initialize throws error if required nodal attributes are miss
   expect_error(
     EpiModel::netsim(fit_with_missing_attr, params, inits, controls),
     regexp = "The following required attributes are missing from the network at initialization: age"
+  )
+
+  # Test initialization with additional attribute (should pass)
+  expect_no_error(
+    EpiModel::netsim(fit_with_additional_attr, params, inits, controls)
   )
 })
