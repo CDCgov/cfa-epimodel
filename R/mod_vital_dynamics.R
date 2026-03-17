00782c876960861c89ebebffe64a0d4083d8a1b0
Emily Pollock <ruu7@AZ1CFA1VSWL-39.ext.cdc.gov>
2026-03-17 09:57:11 -0600

    rm olderpartner tracking, variable name updates

--- R/mod_vital_dynamics.R
+++ R/mod_vital_dynamics.R
@@ -19,21 +19,19 @@ mod_aging <- function(dat, at) {
   # Calc Updated Age Attributes
   age <- get_attr(dat, "age")
   age_group <- get_attr(dat, "age_group")
-  age_group_width <- get_param(dat, "age_group_width")
-  entry_age <- get_param(dat, "entry_age")
-  exit_age <- get_param(dat, "exit_age")
   units <- get_param(dat, "units_per_year")
 
   # Update age and age_group vectors
   age <- age + (1 / units)
-  ngrps <- ceiling((exit_age - entry_age) / age_group_width)
-  for (i in seq_len(ngrps)) {
-    nodes_in_group <- which(
-      age >= (entry_age + (age_group_width * (i - 1))) &
-        age < (entry_age + (age_group_width * i))
-    )
-    age_group[nodes_in_group] <- i
-  }
+  age_group <- dplyr::case_when(
+    age < 50 & age >= 45 ~ 7,
+    age < 45 & age >= 40 ~ 6,
+    age < 40 & age >= 35 ~ 5,
+    age < 35 & age >= 30 ~ 4,
+    age < 30 & age >= 25 ~ 3,
+    age < 25 & age >= 19 ~ 2,
+    age < 19 ~ 1
+  )
 
   # Update Attributes
   dat <- set_attr(dat, "age", age)
@@ -54,18 +52,9 @@ mod_departures <- function(dat, at) {
   active <- get_attr(dat, "active")
   exitTime <- get_attr(dat, "exitTime")
   age <- get_attr(dat, "age")
-  status <- get_attr(dat, "status")
 
   ## Parameters
   exit_age <- get_param(dat, "exit_age")
-
-  ## Double check that entry age and attr.rules age are consistent
-  ## Do this here because we currently use default epimodel arrival module
-  entry_age <- get_param(dat, "entry_age")
-  attr_rules_age <- get_control(dat, "attr.rules")$age
-  if (!is.null(attr_rules_age) && entry_age != attr_rules_age) {
-    stop("entry_age parameter and attr.rules age value must be the same.")
-  }
 
   ## if we had ASMR we would add that here
 
@@ -81,22 +70,6 @@ mod_departures <- function(dat, at) {
     ## Update nodal attributes
     active[idsDept] <- 0
     exitTime[idsDept] <- at
-
-    ## If departing nodes are in main relationship,
-    ## Set partner's olderpartner attr to 1
-    el <- get_edgelist(dat = dat, network = 1) # get edgelist
-    rels1 <- which(el[, 1] %in% idsDept) # any departing id in row 1
-    rels2 <- which(el[, 2] %in% idsDept) # any departing id in row 2
-    allrels <- c(rels1, rels2) # combine
-
-    if (length(allrels) > 0) {
-      allParts <- el[allrels, ] # get all IDs (departing and partner)
-      idsParts <- setdiff(allParts, idsDept) # extract partner IDs
-      # Update Partner Attribute
-      olderpartner <- get_attr(dat, "olderpartner")
-      olderpartner[idsParts] <- 1
-      dat <- set_attr(dat, "olderpartner", olderpartner)
-    }
   }
 
   ## Reset attr
@@ -146,13 +119,13 @@ mod_arrivals <- function(dat, at) {
     if (nArrivals <= 5) {
       # for small nArrivals, sample individually
       # 5 is arbitrary cutoff but seems to work well in testing
-      arrivalSex <- sample(
+      arrival_sex <- sample(
         c(0, 1),
         nArrivals,
         prob = c(1 - female_prob, female_prob),
         replace = TRUE
       )
-      arrivalRace <- sample(
+      arrival_race <- sample(
         race_names,
         nArrivals,
         prob = race_probs,
@@ -160,12 +133,12 @@ mod_arrivals <- function(dat, at) {
       )
     } else {
       # use base EpiModel apportion_lr function if nArrivals > 5
-      arrivalSex <- apportion_lr(
+      arrival_sex <- apportion_lr(
         nArrivals,
         c(0, 1),
         c(1 - female_prob, female_prob)
       )
-      arrivalRace <- apportion_lr(nArrivals, race_names, race_probs)
+      arrival_race <- apportion_lr(nArrivals, race_names, race_probs)
     }
 
     ## Update attributes for new arrivals
@@ -174,9 +147,8 @@ mod_arrivals <- function(dat, at) {
     dat <- append_attr(dat, "infTime", NA, nArrivals)
     dat <- append_attr(dat, "age", entry_age, nArrivals)
     dat <- append_attr(dat, "age_group", 1, nArrivals)
-    dat <- append_attr(dat, "race", arrivalRace, nArrivals)
-    dat <- append_attr(dat, "female", arrivalSex, nArrivals)
-    dat <- append_attr(dat, "olderpartner", 0, nArrivals)
+    dat <- append_attr(dat, "race", arrival_race, nArrivals)
+    dat <- append_attr(dat, "female", arrival_sex, nArrivals)
   }
 
   ## Summary statistics
