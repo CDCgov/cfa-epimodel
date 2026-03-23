b574e47f997d4c0dc52af7073a096aed339ae9d7
Emily Pollock <ruu7@cdc.gov>
2026-03-23 14:58:22 -0600

    add recovery mod and docs

---
+++ R/mgen_recovery.R
@@ -0,0 +1,64 @@
+#' @title Recover from M. genitalium infection
+#'
+#' @description Evaluates eligible infected persons for recovery
+#' based on sex-specific rates, resets infection attrs if recovered
+#'
+#' @inheritParams vitals
+#' @importFrom EpiModel get_attr set_attr set_epi get_param
+#'
+#' @export
+mod_mgen_recovery <- function(dat, at) {
+
+  # Parameters
+  rec_rate_m <- 1 / get_param(dat, "inf_dur_m")
+  rec_rate_f <- 1 / get_param(dat, "inf_dur_f")
+  rec_state <- get_param(dat, "rec_state")
+
+  # Attributes
+  active <- get_attr(dat, "active")
+  status <- get_attr(dat, "status")
+  inf_time <- get_attr(dat, "inf_time")
+  rec_time <- get_attr(dat, "rec_time")
+  female <- get_attr(dat, "female")
+  sympt <- get_attr(dat, "sympt")
+
+
+  # Setup initial vectors
+  n_recov <- n_recov_m <- n_recov_f <- 0
+  ids_elig <- which(active == 1 & status == "i")
+  n_elig <- length(ids_elig)
+
+  # For now, assume exponential dist for recovery
+  # leaving inf_dur calc here in case that changes
+  inf_dur <- at - inf_time[active == 1 & status == "i"]
+  inf_dur[inf_dur == 0] <- 1
+
+  if (n_elig > 0) {
+    female_vec <- female[ids_elig]
+    rate_vec <- ifelse(female_vec == 1, rec_rate_f, rec_rate_m)
+    recov_vec <- which(rbinom(n_elig, 1, rate_vec) == 1)
+    if (length(recov_vec) > 0) {
+      ids_recov <- ids_elig[recov_vec]
+      n_recov <- length(ids_recov)
+      n_recov_f <- sum(female[ids_recov] == 1)
+      n_recov_m <- sum(female[ids_recov] == 0)
+      status[ids_recov] <- rec_state
+      sympt[ids_recov] <- NA
+      rec_time[ids_recov] <- at
+    }
+  }
+
+  # Update attrs
+  dat <- set_attr(dat, "status", status)
+  dat <- set_attr(dat, "sympt", sympt)
+  dat <- set_attr(dat, "rec_time", rec_time)
+
+  # Update epi
+  flow_name <- paste0("i", rec_state, ".flow")
+  dat <- set_epi(dat, flow_name, at, n_recov)
+  dat <- set_epi(dat, paste0(flow_name, ".female0"), at, n_recov_m)
+  dat <- set_epi(dat, paste0(flow_name, ".female1"), at, n_recov_f)
+
+  # Return dat
+  dat
+}
--- R/mod_infection.R
+++ R/mod_infection.R
@@ -1,9 +1,12 @@
 #' @title Model Infection between Partners
 #'
-#' @description Disease transmission module accounting for age-specific activity rates
+#' @description Disease transmission module accounting for
+#' age-specific activity rates and increased transmission prob
+#' for symptomatic persons
 #'
 #' @inheritParams vitals
-#' @importFrom EpiModel get_attr set_attr set_epi get_param set_transmat discord_edgelist
+#' @importFrom EpiModel get_attr set_attr set_epi get_param
+#' set_transmat discord_edgelist
 #'
 #' @export
 
@@ -15,7 +18,7 @@ mod_infection <- function(dat, at) {
   # Variables ---------------------------------------------------------------
 
   active <- get_attr(dat, "active")
-  infTime <- get_attr(dat, "infTime")
+  inf_time <- get_attr(dat, "inf_time")
   status <- get_attr(dat, "status")
   sympt <- get_attr(dat, "sympt")
   female <- get_attr(dat, "female")
@@ -74,16 +77,16 @@ mod_infection <- function(dat, at) {
 
   # Process -----------------------------------------------------------------
   # Vector of infected and susceptible IDs
-  idsInf <- which(active == 1 & status == "i")
-  nActive <- sum(active == 1)
-  nElig <- length(idsInf)
+  ids_inf <- which(active == 1 & status == "i")
+  n_active <- sum(active == 1)
+  n_elig <- length(ids_inf)
 
   # Initialize vectors
   # G2 = female postscript (female attr == 1)
-  nInf <- nInfG2 <- totInf <- 0
+  n_inf <- n_inf_g2 <- tot_inf <- 0
 
   # If some infected AND some susceptible, then proceed
-  if (nElig > 0 && nElig < nActive) {
+  if (n_elig > 0 && n_elig < n_active) {
     # Get discordant edgelist
     del_list <- lapply(
       seq_len(dat$num.nw),
@@ -96,9 +99,10 @@ mod_infection <- function(dat, at) {
 
     # If some discordant edges, then proceed
     if (NROW(del) > 0) {
+      #browser()
       # Infection duration to at
-      del$infDur <- at - infTime[del$inf]
-      del$infDur[del$infDur == 0] <- 1
+      del$inf_dur <- at - inf_time[del$inf]
+      del$inf_dur[del$inf_dur == 0] <- 1
 
       # Calculate transmission rates based on sex & symptom status
       del$sympt_modifier <-
@@ -115,14 +119,14 @@ mod_infection <- function(dat, at) {
 
       # Add age-group specific act rates
       # Using age of youngest partner in partnerships
-      lact_rate <- length(act_rate_vec)
-      if (lact_rate == 1) {
-        del$actRate <- act_rate_vec
+      l_act_rate <- length(act_rate_vec)
+      if (l_act_rate == 1) {
+        del$act_rate <- act_rate_vec
       } else {
         age_inf <- age_group[del$inf]
         age_sus <- age_group[del$sus]
         age_youngest <- ifelse(age_inf < age_sus, age_inf, age_sus)
-        del$actRate <- act_rate_vec[age_youngest]
+        del$act_rate <- act_rate_vec[age_youngest]
       }
 
       # STILL TO BE IMPLEMENTED
@@ -141,7 +145,7 @@ mod_infection <- function(dat, at) {
 
       # Calculate final transmission probability per timestep
       del$finalProb <- 1 -
-        (1 - (del$transProb * (1 - del$condFinal)))^del$actRate
+        (1 - (del$transProb * (1 - del$condFinal)))^del$act_rate
 
       # Randomize transmissions and subset df
       transmit <- stats::rbinom(nrow(del), 1, del$finalProb)
@@ -151,37 +155,41 @@ mod_infection <- function(dat, at) {
       idsNewInf <- unique(del$sus)
       status[idsNewInf] <- "i"
       dat <- set_attr(dat, "status", status)
-      infTime[idsNewInf] <- at
-      dat <- set_attr(dat, "infTime", infTime)
+      inf_time[idsNewInf] <- at
+      dat <- set_attr(dat, "inf_time", inf_time)
+      # currenly, all infs are symptomatic
+      sympt[idsNewInf] <- 1
+      dat <- set_attr(dat, "sympt", sympt)
 
       # Count new infections
       female_attrs <- unique(female)
       if (!all(female_attrs %in% c(0, 1))) {
         stop(
-          "Female attribute must be coded as 0/1 only, which is required for sex-stratified epi slots.",
+          "Female attribute must be coded as 0/1 only,
+          which is required for sex-stratified epi slots.",
           call. = FALSE
         )
       }
       ## Calculate new infections among each sex explicitly by code:
       ## si.flow.female0 corresponds to new infs among female == 0
       ## si.flow.female1 corresponds to new infs among female == 1
-      nInf <- sum(female[idsNewInf] == 0, na.rm = TRUE)
-      nInfG2 <- sum(female[idsNewInf] == 1, na.rm = TRUE)
-      totInf <- nInf + nInfG2
+      n_inf <- sum(female[idsNewInf] == 0, na.rm = TRUE)
+      n_inf_g2 <- sum(female[idsNewInf] == 1, na.rm = TRUE)
+      tot_inf <- n_inf + n_inf_g2
     } # end some discordant edges condition
   } # end some active discordant nodes condition
 
   # Output ------------------------------------------------------------------
 
   # Save transmission matrix
-  if (totInf > 0) {
+  if (tot_inf > 0) {
     dat <- set_transmat(dat, del, at)
   }
 
   ## Save incidence vector
-  dat <- set_epi(dat, "si.flow", at, totInf)
-  dat <- set_epi(dat, "si.flow.female0", at, nInf)
-  dat <- set_epi(dat, "si.flow.female1", at, nInfG2)
+  dat <- set_epi(dat, "si.flow", at, tot_inf)
+  dat <- set_epi(dat, "si.flow.female0", at, n_inf)
+  dat <- set_epi(dat, "si.flow.female1", at, n_inf_g2)
 
   # Return
   dat
