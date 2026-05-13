12115981c905d7c4bbc2be8cd436800dfb3fa9a9
Emily Pollock <ruu7@cdc.gov>
2026-05-13 11:38:24 -0600

    rename simulation utilities file, add fx for common rbinom flow

--- R/epimodel_utilities.R
+++ R/epimodel_utilities.R
@@ -48,3 +48,28 @@ resimnet_updates_sti <- function(dat, at, network) {
   }
   dat
 }
+
+#' @title Helper Function for Binomial Sampling of Eligible IDs
+#' @description This function takes a vector of eligible IDs and a corresponding
+#' vector of probabilities, and returns a vector of IDs that are "successful"
+#' based on a binomial draw. If there are no eligible IDs, it returns an empty integer vector.
+#' @param elig_ids A vector of eligible IDs (e.g., indices of symptomatic infected individuals).
+#' @param eval_prob A vector of probabilities corresponding to each eligible ID, or a single
+#' probability to be applied to all eligible IDs.
+#' @return A vector of IDs or an empty integer vector.
+#' @export
+get_successful_ids_binom <- function(elig_ids, eval_prob) {
+  l_elig_ids <- length(elig_ids)
+  l_eval_prob <- length(eval_prob)
+  if (length(elig_ids) > 0) {
+    if (l_eval_prob %in% c(1, l_elig_ids)) {
+      elig_ids[rbinom(length(elig_ids), 1, eval_prob) == 1]
+    } else {
+      stop(
+        "Length of eval_prob must be either 1 or equal to the number of eligible IDs."
+      )
+    }
+  } else {
+    integer(0) # return empty integer vector if no eligible IDs
+  }
+}
